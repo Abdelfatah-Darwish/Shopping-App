3ecadcdc335c65f7a7f637cece362832fc1cb7b0
noRan56 <noran.nassef12@gmail.com>
2024-09-24 12:46:24 +0300

    -  add images to assets - :art: add packages to pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/dive.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Dive());
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shopping_app/dive.dart';
 
 import 'package:shopping_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Dive());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
