711eadc024e56b060d59b11733ca2fb11c79d127
Abdelfatah-Darwish <abdelfatahdarwish13@gmail.com>
2024-09-25 15:36:51 +0300

    -change the app name to be "ShoppingApp()" -formate  pubspece.yaml

--- lib/core/theming/colors.dart
+++ lib/core/theming/colors.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class ColorScheme {
-  static const Color pink = Color(0xFFF1768);
-  static const Color black = Color(0xF000000);
-  static const Color green = Color(0xF2F8655);
-  static const Color red = Color(0xFDD2222);
+  static const Color pink = Color(0x0fff1768);
+  static const Color black = Color(0x0f000000);
+  static const Color green = Color(0x0f2f8655);
+  static const Color red = Color(0x0fdd2222);
   static const Color Grey = Color.fromRGBO(85, 85, 85, 0.97);
   static const Color darkGrey = Color.fromRGBO(0, 0, 0, 0.5);
   static const Color lightGrey = Color.fromRGBO(121, 121, 121, 1);
