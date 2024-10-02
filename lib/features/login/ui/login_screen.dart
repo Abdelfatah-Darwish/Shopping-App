5e06b3f865c1e46d888c06cc5e656e20934f770f
Abdelfatah-Darwish <abdelfatahdarwish13@gmail.com>
2024-10-02 15:14:29 +0300

    ✨Added image to Login Screen.

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shopping_app/delete_this_after_merge/theming/text_styles.dart';
+import 'package:shopping_app/delete_this_after_merge/widgets/spacing.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,10 +13,28 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-          child: Center(
-        child: Text('Login  Screen!!'),
+          child: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Text(
+                'Welcome back',
+                style: TextStyles.font24BlackRegular
+                    .copyWith(color: const Color(0xFF000000)),
+              ),
+              verticalSpace(16),
+              Image.asset(
+                'assets/images/Tablet login-pana 1.png',
+                height: 230.h,
+                width: 230.w,
+              ),
+              verticalSpace(19),
+              
+            ],
+          ),
+        ),
       )),
     );
   }
