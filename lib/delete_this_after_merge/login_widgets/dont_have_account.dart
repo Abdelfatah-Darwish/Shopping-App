2261bd36c789072d5dc4f01a1e0ba83b8dd8a69b
Abdelfatah-Darwish <abdelfatahdarwish13@gmail.com>
2024-10-02 19:27:39 +0300

    Feat 🚀 Updating LoginScreen UI (Added  Don't Have Account Text)

--- lib/delete_this_after_merge/login_widgets/dont_have_account.dart
+++ lib/delete_this_after_merge/login_widgets/dont_have_account.dart
@@ -13,7 +13,8 @@ class DontHaveAccountText extends StatelessWidget {
         children: [
           TextSpan(
             text: 'Don\'t have an account?',
-            style: TextStyles.font18BlackRegular,
+            style: TextStyles.font18BlackRegular
+                .copyWith(color: const Color(0xFF000000)),
           ),
           TextSpan(
             text: ' Sign Up',
--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shopping_app/delete_this_after_merge/login_widgets/dont_have_account.dart';
 import 'package:shopping_app/delete_this_after_merge/theming/text_styles.dart';
 import 'package:shopping_app/delete_this_after_merge/widgets/spacing.dart';
 import 'package:shopping_app/delete_this_after_merge/widgets/text_button.dart';
@@ -21,67 +22,73 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              Text(
-                'Welcome back',
-                style: TextStyles.font24BlackRegular
-                    .copyWith(color: const Color(0xFF000000)),
-              ),
-              verticalSpace(16),
-              Image.asset(
-                'assets/images/Tablet login-pana 1.png',
-                height: 230.h,
-                width: 230.w,
-              ),
-              verticalSpace(19),
-              Form(
-                key: formKey,
-                child: Column(
-                  children: [
-                    AppTextFormField(
-                      hintText: 'E-mail / phone number',
-                      inputTextStyle: TextStyles.font18BlackRegular,
-                      validator: (value) {},
-                    ),
-                    verticalSpace(19),
-                    AppTextFormField(
-                      hintText: 'Password',
-                      validator: (value) {},
-                      isObscureText: isObscureText,
-                      suffixIcon: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            isObscureText = !isObscureText;
-                          });
-                        },
-                        child: Icon(
-                          isObscureText
-                              ? Icons.visibility_off
-                              : Icons.visibility,
+          child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 21.h),
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Text(
+                  'Welcome back',
+                  style: TextStyles.font24BlackRegular
+                      .copyWith(color: const Color(0xFF000000)),
+                ),
+                verticalSpace(16),
+                Image.asset(
+                  'assets/images/Tablet login-pana 1.png',
+                  height: 230.h,
+                  width: 230.w,
+                ),
+                verticalSpace(19),
+                Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      AppTextFormField(
+                        hintText: 'E-mail / phone number',
+                        inputTextStyle: TextStyles.font18BlackRegular,
+                        validator: (value) {},
+                      ),
+                      verticalSpace(19),
+                      AppTextFormField(
+                        hintText: 'Password',
+                        validator: (value) {},
+                        isObscureText: isObscureText,
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              isObscureText = !isObscureText;
+                            });
+                          },
+                          child: Icon(
+                            isObscureText
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Align(
-                alignment: Alignment.topRight,
-                child: Text(
-                  'Forgot password?',
-                  style: TextStyles.font14PinkRegular
-                      .copyWith(color: const Color(0xFFFF1768)),
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Text(
+                    'Forgot password?',
+                    style: TextStyles.font14PinkRegular
+                        .copyWith(color: const Color(0xFFFF1768)),
+                  ),
                 ),
-              ),
-              const RememberMeCheckbox(),
-              verticalSpace(16),
-              AppTextButton(
+                const RememberMeCheckbox(),
+                verticalSpace(16),
+                AppTextButton(
                   buttonText: 'Sign In',
                   textStyle: TextStyles.font18WhiteRegular,
-                  onPressed: () {}),
-            ],
+                  onPressed: () {},
+                ),
+                verticalSpace(18),
+                const DontHaveAccountText(),
+              ],
+            ),
           ),
         ),
       )),
