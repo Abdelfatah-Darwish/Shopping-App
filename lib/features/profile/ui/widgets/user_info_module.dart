d1c7e8f32c40175965da42e4b00fc46a91d1a78f
noRan56 <noran.nassef12@gmail.com>
2024-10-09 00:36:49 +0300

    - adding module user info to show data in profile screen

---
+++ lib/features/profile/ui/widgets/user_info_module.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shopping_app/core/theming/text_styles.dart';
+
+class UserInFormationSection extends StatelessWidget {
+  const UserInFormationSection(
+      {super.key,
+      required this.title,
+      required this.AssetsPath,
+      this.subtitle});
+  final String title;
+  final String AssetsPath;
+  final Widget? subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        leading: Image.asset(
+          'assets/images/${AssetsPath}',
+          width: 24.w,
+          height: 24.h,
+        ),
+        title: Text(
+          title,
+          style: TextStyles.font15BlackSemiBold,
+        ),
+        subtitle: subtitle,
+        trailing: IconButton(
+            icon: Image.asset(
+              'assets/images/arrow-right.png',
+              width: 24.w,
+              height: 24.h,
+            ),
+            onPressed: () {}));
+  }
+}
