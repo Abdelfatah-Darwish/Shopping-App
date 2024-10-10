3851bd5d7016c5ecfba28c8f33b0b39464d7062d
Abdelfatah-Darwish <abdelfatahdarwish13@gmail.com>
2024-10-10 04:28:24 +0300

    ➕ Adding a dependency firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/core/routing/app_router.dart';
 import 'package:shopping_app/dive.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(Dive(
     appRouter: AppRouter(),
   ));
