0b6902c51f39735122f764e12fe76ab8dab5a418
Abdelfatah-Darwish <abdelfatahdarwish13@gmail.com>
2024-10-10 16:50:54 +0300

    - update build.gradle minSdk

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/core/routing/app_router.dart';
 import 'package:shopping_app/dive.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-void main() async{
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-);
+
+
+
+void main()  {
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
   runApp(Dive(
     appRouter: AppRouter(),
   ));
