43bbc947806fb931341ff366085a926d44283add
Abdelfatah-Darwish <abdelfatahdarwish13@gmail.com>
2024-10-12 01:53:53 +0300

    🐛 Fixing a bugs ( merge branch and solve the conflicts )

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -3,4 +3,7 @@ class Routes {
   static const String signUpScreen = '/signUpScreen';
   static const String homeScreen = '/homeScreen';
   static const String authenticationScreen = '/authenticationScreen';
+  static const String detailsProductScreen = '/datailsProductScreen';
+  static const String profileScreen = '/profileScreen';
+  static const String wishListScreen = '/wishListScreen';
 }
