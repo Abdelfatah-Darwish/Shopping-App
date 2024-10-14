123ea62a7334fcec536c0273ad461af54e39f07a
Abdelfatah-Darwish <abdelfatahdarwish13@gmail.com>
2024-10-15 00:42:52 +0300

    ✨ Introducing new features( Checkout Screen ) feat  🎨 Added Checkout to AppRouter.

---
+++ lib/features/checkout/ui/checkout_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CheckoutScreen extends StatefulWidget {
+  const CheckoutScreen({super.key});
+
+  @override
+  State<CheckoutScreen> createState() => _CheckoutScreenState();
+}
+
+class _CheckoutScreenState extends State<CheckoutScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Text('CheckoutScreen Screen'),
+      ),
+    );
+  }
+}
