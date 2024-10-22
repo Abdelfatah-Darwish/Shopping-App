2a7d5e6658399dd3c607f733514a0211fa0ac927
Abdelfatah-Darwish <abdelfatahdarwish13@gmail.com>
2024-10-23 02:00:56 +0300

    feat( checkout done ) 🎨 add duration to show this screen then go back to home screen.

--- lib/features/checkout/ui/screens/checkout_done_screen.dart
+++ lib/features/checkout/ui/screens/checkout_done_screen.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lottie/lottie.dart';
+import 'package:shopping_app/core/routing/routes.dart'; // Make sure to import your routing
 import 'package:shopping_app/core/theming/text_styles.dart';
 
-class CheckoutDoneScreen extends StatelessWidget {
+class CheckoutDoneScreen extends StatefulWidget {
   const CheckoutDoneScreen({super.key});
+
+  @override
+  State<CheckoutDoneScreen> createState() => _CheckoutDoneScreenState();
+}
+
+class _CheckoutDoneScreenState extends State<CheckoutDoneScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // After 30 seconds, navigate back to the home screen
+    Future.delayed(const Duration(milliseconds: 1890), () {
+      Navigator.pushReplacementNamed(context, Routes.homeScreen);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
