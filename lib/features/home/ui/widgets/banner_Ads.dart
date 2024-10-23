ef5fc6f4826ff470ad03a782840b973ca4074ea8
noRan56 <noran.nassef12@gmail.com>
2024-10-24 00:02:07 +0300

    - adding CarouselSlider to ad bannar

--- lib/features/home/ui/widgets/banner_Ads.dart
+++ lib/features/home/ui/widgets/banner_Ads.dart
@@ -1,24 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class AdBanner extends StatelessWidget {
+class AdBanner extends StatefulWidget {
   const AdBanner({super.key});
 
   @override
+  State<AdBanner> createState() => _AdBannerState();
+}
+
+class _AdBannerState extends State<AdBanner> {
+  final List<String> imageList = [
+    'assets/images/adBanner.png',
+    'assets/images/adBanner.png',
+    'assets/images/adBanner.png',
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-        width: double.infinity,
+    return CarouselSlider(
+      options: CarouselOptions(
         height: 175.h,
-        padding: EdgeInsets.symmetric(
-          horizontal: 16.w,
-          vertical: 16.h,
-        ),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(24.0),
-          image: const DecorationImage(
-            image: AssetImage('assets/images/adBanner.png'),
-            fit: BoxFit.cover,
-          ),
-        ));
+        autoPlay: true,
+        enlargeCenterPage: true,
+        aspectRatio: 16 / 9,
+        viewportFraction: 0.8,
+      ),
+      items: imageList.map((imagePath) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+              width: 300.w,
+              margin: const EdgeInsets.symmetric(horizontal: 5.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.white,
+                    spreadRadius: 2,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset(
+                  imagePath,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
+          },
+        );
+      }).toList(),
+    );
   }
 }
