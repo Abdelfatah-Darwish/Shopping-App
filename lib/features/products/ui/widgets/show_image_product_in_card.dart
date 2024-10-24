e696b3a4ef1ed9c83c39c2ccc8549ef4d5ef5270
noRan56 <noran.nassef12@gmail.com>
2024-10-24 04:01:48 +0300

    - edit favicon

--- lib/features/products/ui/widgets/show_image_product_in_card.dart
+++ lib/features/products/ui/widgets/show_image_product_in_card.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/core/theming/colors.dart';
 import 'package:shopping_app/features/products/data/model/products_model.dart';
+import 'package:shopping_app/features/wishlist/data/sqlflite_for_wishlist.dart';
+import 'package:shopping_app/features/wishlist/ui/widgets/app_favourite_icon.dart'; // Database helper import
 
 class ImageProductsCard extends StatefulWidget {
   final Product product;
@@ -11,15 +13,9 @@ class ImageProductsCard extends StatefulWidget {
 }
 
 class _ImageProductsCardState extends State<ImageProductsCard> {
-  // List to track whether each item is selected
-  late List<bool> isSelected;
-  //late int index;
-
   @override
   void initState() {
     super.initState();
-    // Initialize the isSelected list with false for each item
-    isSelected = List<bool>.filled(widget.product.image.length, false);
   }
 
   @override
@@ -37,32 +33,20 @@ class _ImageProductsCardState extends State<ImageProductsCard> {
             width: double.infinity,
           ),
         ),
+
         Positioned(
           top: 8,
           left: 8,
-          child: CircleAvatar(
-            backgroundColor: Colors.white,
-            child: IconButton(
-              onPressed: () {},
-              icon: Image.asset('assets/images/love.png'),
-            ),
-          ),
+          child: AppFavouriteIcon(),
         ),
+        // Add to cart button
         Positioned(
           bottom: 4,
           right: 8,
           child: CircleAvatar(
-            backgroundColor: isSelected[widget.product.id]
-                ? ColorsManager.pink
-                : ColorsManager.white,
+            backgroundColor: ColorsManager.white,
             child: IconButton(
-              onPressed: () {
-                setState(() {
-                  // Toggle selection state
-                  isSelected[widget.product.id] =
-                      !isSelected[widget.product.id];
-                });
-              },
+              onPressed: () {},
               icon: const Icon(Icons.add_shopping_cart),
             ),
           ),
